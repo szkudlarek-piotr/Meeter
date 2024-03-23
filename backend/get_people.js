4520d07bd8f21d8f90659f3c7c46de63d77cb049
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-03-23 14:57:33 +0100

    initial commit

--- backend/get_people.js
+++ backend/get_people.js
@@ -1,27 +1,29 @@
-console.log("test0")
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-host     : 'localhost',
-user     : 'root',
-database : 'test'
-});
-console.log("test1")
-let all_humans = []
-connection.connect();
-console.log("Dupa")
-let test_query = "SELECT * FROM party_people;"
-connection.query(test_query, function (error, results, fields) {
-if (error)   throw error;
-for (let i=0; i<results.length; i++) {
-    //console.log(i)
-    let photo_path = `./photos/${results[i].ID}.jpg`
-    let new_human = {"id": results[i].ID, "name": results[i].name, "surname": results[i].surname, "lives_in": results[i].lives_where, "gender": results[i].gender, "facebook_link": results[i].fb_link, "clique": results[i].klika_id, "photography_path": photo_path}
-    let identifier = `${results[i].name}_${results[i].surname}_${results[i].ID}`
-    console.log(new_human)
-    all_humans[identifier]=new_human
+function getAllPeople() {
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    database : 'test'
+    });
+    let all_humans = []
+    connection.connect();
+    let test_query = "SELECT * FROM party_people;"
+    connection.query(test_query, function (error, results, fields) {
+    if (error)   throw error;
+    for (let i=0; i<results.length; i++) {
+        new_human = {}
+        let photo_path = `./photos/${results[i].ID}.jpg`
+        new_human["id"] = results[i].ID
+        new_human["name"] = results[i].name
+        new_human["surname"] = results[i].surname
+        new_human["lives_in"] = results[i].lives_in
+        new_human["facebook_link"] = results[i].fb_link
+        new_human["clique"] = results[i].klika_id
+        new_human["photography_path"] = photo_path
+        all_humans.push(new_human)
+    }
+    });
+    connection.end();
+    return JSON.stringify(all_humans)
 }
-
-console.log(all_humans)
-});
-connection.end();
-return all_humans
+module.exports = getAllPeople
