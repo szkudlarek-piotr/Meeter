210dbfe88051a1c05e20b85b6586ad261eaa137a
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-04-02 00:28:26 +0200

    collective commit

--- getLeftMenu.js
+++
@@ -1,5 +0,0 @@
-function createLeftMenu() {
-    //póki co testuję to na prawym menu, stąd ten mindfuck
-    let leftMenu = document.getElementsByClassName("right-menu")[0]
-    console.log(leftMenu)
-}
