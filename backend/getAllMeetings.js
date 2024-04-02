eabdadca2cd7f771d18cd0aba151d620046ac315
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-04-02 17:51:50 +0200

    When a person has no photo, it shows a drawing of "anonymous" person

--- backend/getAllMeetings.js
+++ backend/getAllMeetings.js
@@ -12,21 +12,27 @@ const pool = mysql.createPool({
 
 async function getAllMeetings() {
     let jsonOfDays = {}
-    let [ dataFromDatabase ] = await pool.query("SELECT meetings.meeting_date, meeting_human.human_id, party_people.name, party_people.surname FROM meeting_human JOIN meetings ON meetings.ID = meeting_human.meeting_id JOIN party_people ON meeting_human.human_id = party_people.id")
+    let [ dataFromDatabase ] = await pool.query("SELECT meetings.meeting_date, meetings.title_after_hover, meeting_human.human_id, party_people.name, party_people.surname FROM meeting_human JOIN meetings ON meetings.ID = meeting_human.meeting_id JOIN party_people ON meeting_human.human_id = party_people.id")
     //I am adding hours to a date due to timezone issues
     for (meeting of dataFromDatabase) {
         const oldTime = meeting["meeting_date"]
         const oldTimeFormated = new Date(oldTime)
         const newTime = date.addHours(oldTimeFormated, 13)
-        console.log("test")
+        let photoDir = path.join(__dirname, "photos", meeting.human_id + ".jpg")
+        if (!fs.existsSync(photoDir)) {
+            photoDir = path.join(__dirname, "photos", "anonymous.jpg")
+        }
+        
         if (!jsonOfDays.hasOwnProperty(newTime)) {
-            jsonOfDays[newTime] = {"names": [`${meeting.name} ${meeting.surname}`], "photos": [`${path.join(__dirname, "photos", meeting.human_id + ".jpg")}`]}
+            jsonOfDays[newTime] = {"names": [`${meeting.name} ${meeting.surname}`], "photos": [`${photoDir}`]}
+            jsonOfDays[newTime]["title_after_hover"] = meeting.title_after_hover
         }
         else {
             jsonOfDays[newTime]["names"].push(`${meeting.name} ${meeting.surname}`)
-            jsonOfDays[newTime]["photos"].push(`${path.join(__dirname, "photos", meeting.human_id + ".jpg")}`)
+            jsonOfDays[newTime]["photos"].push(`${photoDir}`)
         }
     }
+    console.log(jsonOfDays)
     return jsonOfDays
 }
 module.exports = getAllMeetings
