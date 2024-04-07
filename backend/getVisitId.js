3e87877385e491fd1f20969ac7bd7e50618ce2a8
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-04-07 22:35:36 +0200

    returns ID of the visit (useful if you want to add them just adter creating visit)

---
+++ backend/getVisitId.js
@@ -0,0 +1,19 @@
+var mysql      = require('mysql2');
+const datetime = require('date-and-time')
+var dotenv = require('dotenv');
+dotenv.config()
+const pool = mysql.createPool({
+    host     : process.env.host,
+    user     : process.env.MYSQL_USER,
+    database : process.env.MYSQL_DATABASE
+}).promise()
+async function getVisitId(dateFromRequest, duration, description) {
+    const dateInProperFormat = new Date(dateFromRequest)
+    const modifiedTime = datetime.addHours(dateInProperFormat, 12)
+    const writtenRequest = "SELECT visit_id FROM visits WHERE DATE(visit_date) = DATE(?) AND visit_duration = ? AND title_afte_hover = ?"
+    const [ idOfMeeting ] = await pool.query(writtenRequest, [dateInProperFormat, duration,  description])
+    //console.log(writtenRequest)
+    return idOfMeeting
+}
+//getVisitId("2024-04-25", "1", ":Knskdjn")
+module.exports = getVisitId
