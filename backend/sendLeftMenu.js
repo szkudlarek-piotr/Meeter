656c831707a1fb91bb307cb1be051b09d9457b03
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-04-14 13:08:50 +0200

    Added a separate html document for watching all the cliques

--- backend/sendLeftMenu.js
+++ backend/sendLeftMenu.js
@@ -1,7 +1,7 @@
 function sendLeftMenu() {
     const menuButtonsArray = [{"id":"mainPage","text": "Strona główna", "onclick": "showMainPage()"}, {"id":"addNewHuman","href": "add_human.html", "text": "Dodaj nowego człowieka"},
      {"id": "calendar","href": "calendar.html", "text": "Zobacz kalendarz"},
-      {"id": "showAllCliques","onclick": "all_people.html", "text": "Pokaż wszystkie kliki"},
+      {"id": "showAllCliques","href": "all_cliques.html", "text": "Pokaż wszystkie kliki"},
        {"id": "showAllPeople","href": "all_people.html", "text": "Zobacz wszystkich ludzi"}, 
        {"id": "addNewMeeting", "href": "add_meeting.html", "text": "Dodaj nowe spotanie"}, 
        {"id": "addNewClique", "href": "add_new_clique.html", "text": "Dodaj nową klikę"}, 
