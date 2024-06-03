e9c382a7e13d7845c510f6320b5ab3a5ba216dce
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-06-03 22:23:19 +0200

    Added adding quotes to the left menu

--- backend/sendLeftMenu.js
+++ backend/sendLeftMenu.js
@@ -5,7 +5,7 @@ function sendLeftMenu() {
        {"id": "showAllPeople","href": "all_humans.html", "text": "Zobacz wszystkich ludzi"}, 
        {"id": "addNewMeeting", "href": "add_meeting.html", "text": "Dodaj nowe spotanie"}, 
        {"id": "addNewClique", "href": "add_new_clique.html", "text": "Dodaj nową klikę"}, 
-       {"id": "addHumansToMeeting", "text": "Dodaj ludzi do spotkania", "href": "add_humans_to_meeting.html"}, {"id": "addVisit", "href": "add_visit.html", "text": "Dodaj wizytę!"}]
+       {"id": "addHumansToMeeting", "text": "Dodaj ludzi do spotkania", "href": "add_humans_to_meeting.html"}, {"id": "addVisit", "href": "add_visit.html", "text": "Dodaj wizytę!", "id": "adQuote", "href": "add-golden-quote.html", "text": "Dodaj Złoty Cytat"}]
     return menuButtonsArray
 }
 module.exports = sendLeftMenu
