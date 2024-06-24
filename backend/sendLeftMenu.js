8e0e3eb7c18194b94ee12d3aab91296359c94146
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-06-24 21:21:28 +0200

    Added several new pages to the menu

--- backend/sendLeftMenu.js
+++ backend/sendLeftMenu.js
@@ -5,7 +5,10 @@ function sendLeftMenu() {
        {"id": "showAllPeople","href": "all_humans.html", "text": "Zobacz wszystkich ludzi"}, 
        {"id": "addNewMeeting", "href": "add_meeting.html", "text": "Dodaj nowe spotanie"}, 
        {"id": "addNewClique", "href": "add-clique.html", "text": "Dodaj nową klikę"}, 
-       {"id": "addHumansToMeeting", "text": "Dodaj ludzi do spotkania", "href": "add_humans_to_meeting.html"}, {"id": "addVisit", "href": "add_visit.html", "text": "Dodaj wizytę!"}, {"id": "addQuote", "href": "add-golden-quote.html", "text": "Dodaj Złoty Cytat"}]
+       {"id": "addHumansToMeeting", "text": "Dodaj ludzi do spotkania", "href": "add_humans_to_meeting.html"},
+        {"id": "addVisit", "href": "add_visit.html", "text": "Dodaj wizytę!"}, 
+        {"id": "addQuote", "href": "add-golden-quote.html", "text": "Dodaj Złoty Cytat"}, 
+        {"id": "addEvent", "href": "add-event.html", "text": "Dodaj wydarzenie"}]
     return menuButtonsArray
 }
 module.exports = sendLeftMenu
