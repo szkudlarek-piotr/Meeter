893bdf2a849575b39a99b94dab4d2b2ecb4c0144
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2024-10-04 08:00:18 +0200

    Refactor of the calendar

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 app = express()
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const getPeople = require('./get_people.js')
 var multer = require('multer')
 var upload = multer({ dest: 'photos/'})
 const addGuestToVisit = require('./addGuestToVisit.js')
 const addVisit = require('./addVisit.js')
-const getWeddingDetails = require('./getSingleWedding.js')
 const getEventsWithCompanion = require('./getEventsWithCompanion.js')
 const getVisitId = require('./getVisitId.js')
 const humanDetails = require('./getHumanDetails.js')
@@ -19,6 +20,7 @@ const getAllMeetingsDates = require('./getAllMeetingsDates.js')
 const getNumberOfVisists = require('./getNumberOfVisits.js')
 const getAllCliques = require('./getAllCliques.js')
 const fixedHumans = require('./fixedHumans.js')
+const singleVisitDetails = require('./getSingleVisit.js')
 const getSingleWedding = require('./getSingleWedding.js')
 const peopleFromString = require('./selectPeopleFromSubstring.js')
 const getHumanClique = require('./addCliquesToHumans.js')
@@ -32,12 +34,14 @@ const meetingsNumber = require('./getNumberOfMeetings.js')
 const getEvents = require('./getEvents.js')
 const addMeeting = require('./addMeeting.js')
 const addEvent = require('./addEvent.js')
+const getNews = require('./getNews.js')
 const getIdFromIdentifiers = require('./getIdFromIdentifiers.js')
 const getMeetingId = require('./getMeetingId.js')
 const addPersonToMeeting = require('./addPersonToMeting.js')
 const weddingPartners = require('./weddingPartnerFromSubsting.js')
 const uploadHumanPhoto = require('./uploadHumanPhoto.js')
 const addPartnerToWedding = require('./addPartnerToWedding.js')
+const wholeCalendar = require('./getAllCalendar.js')
 app.use((err,req,res,next) => {
     console.error(err.stack)
     res.status(500).send('something broke')
@@ -48,6 +52,10 @@ app.get("/people", async (req, res) => {
     res.send(JSON.stringify(people))
 })
 
+app.get("/whole-calendar", async (req, res) => {
+    const daysToAdd = await wholeCalendar()
+    res.send(JSON.stringify(daysToAdd))
+})
 app.get("/meetings-number", async (req, res) => {
     const numbersOfMeetings = await meetingsNumber()
     res.send(JSON.stringify(numbersOfMeetings))
@@ -62,10 +70,21 @@ app.get('/events', async (req, res) => {
     res.send(JSON.stringify(eventsJson))
 })
 
+app.get('/visit-details', async(req, res) => {
+    const visitId = req.query.visit_id
+    const visitJson = await singleVisitDetails(visitId)
+    res.send(JSON.stringify(visitJson))
+})
+
 app.get('/wedding-partner-search', async (req, res) => {
     const substring = req.query.subs
     const potentialPartners = await weddingPartners(substring)
     res.send(potentialPartners)
+})
+
+app.get('/get-news', async (req, res) => {
+    const allNews = await getNews()
+    res.send(JSON.stringify(allNews))
 })
 
 app.get('/single-wedding', async (req, res) => {
@@ -134,13 +153,17 @@ app.get('/add_visits_to_calendar', async (req, res) => {
     res.send(JSON.stringify(daysVisitsJson))
 })
 app.patch('/wedding-partner-change', async(req, res) => {
-    console.log(req.query)
     const weddingId = req.query.wedding
     const partnerId = req.query.partner
-    console.log(`Zamierzam uczynić pannę ${partnerId} twoją partnerką na wesele ${weddingId}.`)
     const addingMessage = await addPartnerToWedding(partnerId, weddingId)
-    console.log(addingMessage)
     res.send(JSON.stringify(addingMessage))
+    //try {
+        //
+        
+    //}
+    //catch (error) {
+        //res.send(error)
+    //}
 })
 app.post('/save-quote', async(req, res) => {
     const authorId = req.query.author
@@ -177,6 +200,9 @@ app.post('/add_guest_to_visit', async (req, res) => {
     }
     
 })
+app.post('/add-clique', async(req, res) => {
+    console.log(req)
+})
 app.post('/add_human_to_meeting', async (req, res) => {
     const meeting = req.query.meeting
     const human = req.query.human
@@ -200,7 +226,14 @@ app.post('/add_human_to_meeting', async (req, res) => {
 })
 
 app.post('/add-event', async (req, res) => {
-    await console.log(req.body)
+    const eventName = req.query.eventName
+    const placeName = req.query.placeName
+    const eventStartDate = req.query.eventStartDate
+    const eventStopDate = req.query.eventStopDate
+    const comingDate = req.query.comingDate
+    const leavingDate = req.query.leavingDate
+    const genericPhotoName = req.query.genericPhotoName
+    const addEventReq = await addEvent(eventName, placeName, eventStartDate, eventStopDate, comingDate, leavingDate, genericPhotoName)
 })
 app.get('/meetings', async (req, res) => {
     const allMeetings = await getMeetings()
@@ -228,7 +261,6 @@ app.get('/meeting_id_from_date', async (req, res) => {
 })
 app.post ('/upload_human_photo', upload.single('photo'), (req, res) => {
     const file = req.file
-    console.log(file)
     const description = req.body.description
     res.send("OK")
 })
@@ -261,7 +293,6 @@ app.post('/add_visit', async (req,res) => {
     const duration = req.query.duration
     const description = req.query.description
     const postRequest = addVisit(date, duration, description)
-    //res.send(postRequest.status)
     res.send(postRequest)
 })
 
---
+++ backend/getAllCalendar.js
@@ -0,0 +1,150 @@
+var mysql      = require('mysql2');
+const path = require('path')
+var dotenv = require('dotenv');
+const fs = require('fs')
+const moment = require('moment');
+dotenv.config()
+const pool = mysql.createPool({
+    host     : process.env.host,
+    user     : process.env.MYSQL_USER,
+    database : process.env.MYSQL_DATABASE
+}).promise()
+
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // losowy indeks od 0 do i
+        [array[i], array[j]] = [array[j], array[i]]; // zamiana elementów miejscami
+    }
+    return array;
+}
+
+async function wholeCalendar(){
+    let daysJson = {}
+    let weddingsQueryText = `SELECT ADDDATE(weddings.date, INTERVAL 12 HOUR) AS wed_date, man_id, woman_id, partner_id, id FROM weddings`
+    const [weddingsQuery] = await pool.query(weddingsQueryText)
+    for (record of weddingsQuery) {
+        let wedDate = record.wed_date
+        const manId = record.man_id
+        const manPhotoPath = path.join(__dirname, "photos", manId + ".jpg") 
+        const womanId = record.woman_id
+        const womanPhotoPath = path.join(__dirname, "photos", womanId + ".jpg") 
+        const weddingId = record.id
+        const partnerId = record.partner_id
+        let partnerPhotoPath = ""
+        if (partnerId != null) {
+            partnerPhotoPath = path.join(__dirname, "photos", partnerId + ".jpg") 
+        }
+        else {
+            partnerPhotoPath = null
+        }
+        daysJson[wedDate] = {"class": "wedding" ,"man": manPhotoPath, "woman": womanPhotoPath, "wedding_id": weddingId, "partnerPhoto": partnerPhotoPath}
+    }
+
+
+    const visitsQueryText = `SELECT visits.visit_id as visit_id, CONCAT(party_people.name, " ", party_people.surname) AS human_name, CONCAT(visit_guest.guest_id, ".jpg") AS photo_name, ADDDATE(visits.visit_date, INTERVAL 10 HOUR) as visit_date, visits.visit_duration as duration, visits.title_afte_hover as on_hover 
+    FROM visit_guest
+    JOIN party_people ON party_people.id = guest_id
+    JOIN visits ON visits.visit_id = visit_guest.visit_id`
+    const [visitsQuery] = await pool.query(visitsQueryText)
+    for (record of visitsQuery) {
+        const startAsMoment = moment(record["visit_date"])
+        const onHover = record["on_hover"]
+        const endAsMoment = startAsMoment.add(record["duration"], "days")
+        for (let day = moment(record["visit_date"]); day.isBefore(endAsMoment); day.add(1, "days")) {
+            if (day in daysJson) {
+                daysJson[day]["photos"].push(path.join(__dirname, "photos", record["photo_name"]))
+            }
+            else {
+                daysJson[day] = {"photos": [path.join(__dirname, "photos", record["photo_name"])], "class": "visit", "visit_id": record.visit_id, "on_hover": onHover}
+            }
+        }
+
+       
+    }
+    const citybrakesQueryText = `SELECT citybreaks.ID as citybreakId, ADDDATE(citybreaks.Date_start, INTERVAL 10 HOUR) as start_date, ADDDATE(citybreaks.Date_stop, INTERVAL 10 HOUR) as stop_date, citybreaks.Place FROM citybreaks;`
+    const [citybrakesReq] = await pool.query(citybrakesQueryText)
+    for (record of citybrakesReq) {
+        const startDate = record.start_date
+        const stopDate = record.stop_date
+        const citybreakId = record.citybreakId.toString()
+        const tripPotosFolderPath = path.join(__dirname, "trips", citybreakId)
+        const listOfTripPhotosNames = fs.readdirSync(tripPotosFolderPath)
+        let listOfTripPhotosPaths = []
+        for (photoName of listOfTripPhotosNames) {
+            listOfTripPhotosPaths.push(path.join(__dirname, "trips", citybreakId, photoName))
+        }
+        for (let checkedDay = moment(startDate); checkedDay.diff(stopDate, 'days') <= 0; checkedDay.add(1, 'days')) {
+            const shuffledPhotosArr = shuffleArray(listOfTripPhotosPaths)
+            daysJson[checkedDay.toDate()]  = {"place": record.Place, "class": "citybreak", "citybreakId": citybreakId, "photos": shuffledPhotosArr}
+        }
+    }
+    const meetingsText = `SELECT ADDDATE(meetings.meeting_date, INTERVAL 10 HOUR) AS meeting_date, meetings.ID as meeting_id, meetings.title_after_hover AS meeting_desc, CONCAT(meeting_human.human_id, ".jpg") as human_photo FROM meeting_human JOIN meetings ON meetings.ID = meeting_human.meeting_id`
+    const [meetingReq] = await pool.query(meetingsText)
+    for (record of meetingReq) {
+        const meetingDate = record.meeting_date
+        const meetingId = record.meeting_id
+        const onhover = record.meeting_desc
+        let photo_to_add = path.join(__dirname, "photos", record.human_photo)
+        if (!(fs.existsSync(photo_to_add))) {
+            photo_to_add = path.join(__dirname, "photos", "anonymous.jpg")
+        }
+        if (!(meetingDate in daysJson)) {
+            daysJson[meetingDate] = {"photos": [photo_to_add], "class": "meeting", "meetingId": meetingId, "onhover": onhover}
+        }
+        else {
+            if (daysJson[meetingDate]["class"] === "meeting") {
+                daysJson[meetingDate]["photos"].push(photo_to_add)
+            }
+        }
+    }
+    const eventQueryText = `SELECT events.id as event_id, ADDDATE(events.meComingDate, interval 10 HOUR) as coming_date, ADDDATE(events.meLeavingDate, INTERVAL 10 HOUR) as leaving_date, CONCAT(events.nameOfEvent, " w miejscu ", events.place) as hover_description, events.Generic_photo as generic_photo, CONCAT(event_companion.human_id, ".jpg") as human_photo_name FROM events LEFT JOIN event_companion ON event_companion.event_id = events.id`
+    const [eventsReq] = await pool.query(eventQueryText)
+    for (record of eventsReq) {
+        const startDate = record.coming_date
+        const stopDate = record.leaving_date
+        const eventId = record.event_id
+        const textOnHover = record.hover_description
+        let humanPhotoName = record.human_photo_name
+        let humanPhotoPath = ""
+        if (humanPhotoName != null) {
+            if (fs.existsSync(path.join(__dirname, "photos", humanPhotoName))) {
+                humanPhotoPath = path.join(__dirname, "photos", humanPhotoName)
+            }
+            else {
+                humanPhotoPath = path.join(__dirname, "photos", "anonymous.jpg")
+            }
+        }
+        if (record.human_photo != null) {
+            humanPhotoPath = path.join(__dirname, "photos", record.human_photo)
+        }
+        let event_photo_path = ""
+        if (record.generic_photo != "") {
+            event_photo_path = path.join(__dirname, "events", record.generic_photo)
+        }
+        let potental_path_jpg = path.join(__dirname, "events", eventId + ".jpg")
+        if (fs.existsSync(potental_path_jpg)) {
+            event_photo_path = potental_path_jpg
+        }
+        let potental_path_png = path.join(__dirname, "events", eventId + ".png")
+        if (fs.existsSync(potental_path_png)) {
+            event_photo_path = potental_path_png
+        }
+        for (let checkedDay = moment(startDate); checkedDay.diff(stopDate, 'days') <= 0; checkedDay.add(1, 'days')) {
+            if (!(checkedDay.toDate() in daysJson)) {
+                daysJson[checkedDay.toDate()] = {"class": "event", "photos": [event_photo_path], "eventDescription": textOnHover}
+                if (humanPhotoName != null) {
+                    daysJson[checkedDay.toDate()]["photos"].push(humanPhotoPath)
+                }
+            }
+            else {
+                if (!(humanPhotoPath in daysJson[checkedDay.toDate()]["photos"])) {
+                    daysJson[checkedDay.toDate()]["photos"].push(humanPhotoPath)
+                }
+            }
+        }
+    }
+    return daysJson
+}
+
+module.exports = wholeCalendar
